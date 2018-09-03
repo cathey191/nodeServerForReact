86a77c0a199f27d34911ca45095a75141d8705c6
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-09-03 21:29:08 +0000

    server setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const data = require('./data/data.json');
+
+const app = express();
+
+app.use(function(req, res, next) {
+	console.log(`${req.method} request for ${req.url}`);
+	next();
+});
+
+app.get('/get.json', function(req, res) {
+	res.json(data);
+});
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+	console.log(`Server is running on port ${app.get('port')}`);
+});
