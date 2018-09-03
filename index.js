547af69c77985dff6ff262aa8c6144e946fd79f6
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-09-03 21:51:25 +0000

    included cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const data = require('./data/data.json');
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 app.use(function(req, res, next) {
 	console.log(`${req.method} request for ${req.url}`);
